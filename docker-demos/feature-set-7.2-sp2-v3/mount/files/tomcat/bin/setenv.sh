ce740a47d7822fe5781aa5c19233f14b47c2d8cd
Jonas Choi <jonas.choi@liferay.com>
2021-12-14 09:24:24 -0800

    Log4Shell JVM property mitigation

--- docker-demos/feature-set-7.2-sp2-v3/mount/files/tomcat/bin/setenv.sh
+++ docker-demos/feature-set-7.2-sp2-v3/mount/files/tomcat/bin/setenv.sh
@@ -1,1 +1,1 @@
-CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.locale.providers=JRE,COMPAT,CLDR -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xms2560m -Xmx4096m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=512m -XX:MetaspaceSize=512m -XX:NewSize=1536m -XX:SurvivorRatio=7"
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.locale.providers=JRE,COMPAT,CLDR -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xms2560m -Xmx4096m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=512m -XX:MetaspaceSize=512m -XX:NewSize=1536m -XX:SurvivorRatio=7 -Dlog4j2.formatMsgNoLookups=true"
---
+++ docker-demos/liferay-dxp-7.3/mount/files/tomcat/bin/setenv.sh
@@ -0,0 +1,1 @@
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF-8 -Djava.locale.providers=JRE,COMPAT,CLDR -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xms2560m -Xmx4096m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=768m -XX:MetaspaceSize=768m -XX:NewSize=1536m -XX:SurvivorRatio=7 -Dlog4j2.formatMsgNoLookups=true"
